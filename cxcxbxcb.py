023bdc8b6c1a4acbffd8f15b6ec4dabfee6d72ae
romchik56 <roman.k.for.games@gmail.com>
2024-10-15 19:03:22 +0300

    Create cxcxbxcb.py

---
+++ cxcxbxcb.py
@@ -0,0 +1,34 @@
+'''a=int(input())
+if a>14:
+    print("Добро пожаловать!")
+elif a<14:
+    print("Вам ещё рано!")
+else:
+    print( "Вам 14 лет! Поздравляю!")'''
+
+'''a=input()
+if a=='secret':
+    print( "Доступ разрешен!")
+else:
+    print("Неверный пароль!")'''
+
+'''a=input()
+b='1234567890'
+c='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+if a in c:
+    print('Это буква!')
+elif a in b:
+    print('Это цифра!')
+else:
+    print('Это не цифра и не буква!')'''
+
+'''a=int(input())
+b=0
+if a>1000:
+    b = a-(a*0.1)
+    print(b)
+elif a>500:
+    b = a-(a*0.05)
+    print(b)
+else:
+    print("Скидки нет")'''
